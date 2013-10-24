6cb03f8de6c4f3844539bf17dc6880ac3d7f7010
Nils Van Geele <nils.vgeele@gmail.com>
2013-10-24 23:30:38 +0200

    Directed graphs with hashes + bugfixes

--- src/directed-graph.rkt
+++ src/directed-graph.rkt
@@ -1,104 +1,92 @@
 #!racket
 
 (provide make-directed-graph
+	 connected?
 	 add-edge
 	 node-data set-node-data
 	 edge-data set-edge-data
 	 for-each-node
 	 for-each-edge)
 
-;; Data in a graph struct is a list of cons cells.
-;; The car of the cell is the data for the node,
-;; the cdr is the list of connected edges.
-;; This list of connected edges again is a list of
-;; cons cells. Car is the `to' node, cdr is the 
-;; data for this edge.
+;; This isn't a "real" graph; a real mathematical
+;; graph has a finite set of nodes/vertices,
+;; whilst this representation allows for a
+;; growing amount of vertices.
 
-(struct directed-graph (data size)
+;; Data in a graph struct is a hash.
+;; For each node there is a cons cell assigned as
+;; value. The car of this cons cell is the data
+;; for this node, the cdr is the hash of outgoing
+;; edges.
+
+;; The hash of outgoing edges has a value assigned
+;; to each node that is connected with the node.
+;; This is the data for this edge. The value can
+;; be empty
+
+(struct directed-graph (data)
 	#:transparent)
 
 (define data directed-graph-data)
-(define size directed-graph-size)
 
 (define (make-directed-graph)
-  (directed-graph '() 0))
+  (directed-graph (hash)))
 
-(define (add-edge graph from to (data '()))
-  (let ((graph (if (> (max from to) (size graph))
-		 (grow-graph graph (max from to))
-		 graph)))
-    (struct-copy directed-graph graph
-		 [data (for/list ([i (build-list (size graph) values)]
-				  [l (data graph)])
-				 (if (= i from)
-				   (cons (car l)
-					 (cons (cons to data) (cdr l)))
-				   l))])))
+(define (connected? graph from to)
+  (let ((edges (hash-ref (data graph) from #f)))
+    (if edges
+      (not (false? (hash-ref (cdr edges) to #f)))
+      #f)))
+
+;; Will add an edge, as long as from and to aren't
+;; connected yet. If a node has no data yet, then
+;; it well just be initialized with empty data.
+(define (add-edge graph from to (new-data '()))
+  (if (connected? graph from to)
+    #f ;; To error or not to error, that is the question
+    (directed-graph (hash-set (data graph) from
+			      (let ((node (hash-ref (data graph)
+						    from (cons '() (hash)))))
+				(cons (car node)
+				      (hash-set (cdr node) to new-data)))))))
 
 (define (node-data graph node)
-  (if (<= node (size graph))
-    (car (list-ref (data graph) node))
-    #f))
+  (let ((data (hash-ref (data graph) node #f)))
+    (if data (car data) #f)))
 
-(define (set-node-data graph node data)
-  (if (<= node (size graph))
-    (struct-copy directed-graph graph
-		 [data (for/list ([i (build-list (size graph) values)]
-				  [l (data graph)])
-				 (if (= i node)
-				   (cons data
-					 (cdr l))
-				   l))])
-    #f)) ;; To error, or not to error, that is the question
+(define (set-node-data graph node new-data)
+  (let ((edges (cdr (hash-ref (data graph) node (cons '() (hash))))))
+    (directed-graph (hash-set (data graph) node
+			      (cons new-data edges)))))
 
 (define (edge-data graph from to)
-  (if (<= (max from to) (size graph))
-    (let ((edges (cdr (list-ref (data graph) from))))
-      (for/or ([edge edges])
-	      (if (= (car edge) to)
-		(cdr edge)
-		#f)))
+  (if (connected? from to)
+    (let ((edges (cdr (hash-ref (data graph) from))))
+      (hash-ref edges to))
     #f))
 
 ;; If the edge doesn't exist, we aren't going
 ;; to create it!
-(define (set-edge-data graph from to data)
-  (if (<= (max from to) (size graph))
-    (struct-copy directed-graph graph
-		 [data (for/list ([i (build-list (size graph))]
-				  [n (data graph)])
-				 (if (= i from)
-				   (cons (car n)
-					 (for/list ([e (cdr n)])
-						   (if (= to (car e))
-						     (cons to data)
-						     e)))
-				   n))])
+(define (set-edge-data graph from to new-data)
+  (if (connected? from to)
+    (let ((node (hash-ref (data graph) from)))
+      (directed-graph (hash-set (data graph) from
+				(cons (car node)
+				      (hash-set (cdr node) to new-data)))))
     #f))
 
 ;; Calls func for each node.
 ;; First argument is the data for that node,
 ;; second argument the list of edges.
 (define (for-each-node graph func)
-  (for ([node (data graph)])
-       (func (car node) (cdr node))))
+  (for ([(n v) (data graph)])
+       (func (car v) (cdr v))))
 
 ;; Calls func for each edge originating from
 ;; the node.
 ;; First argument is data for the edge,
 ;; second the to node.
 (define (for-each-edge graph from func)
-  (if (<= from (size graph))
-    (let ((edges (cdr (list-ref (data graph) from))))
-      (for ([edge edges])
-	   (func (cdr edge) (car edge))))
-    #f))
-
-;;; Changes the size of the adjacency list
-(define (grow-graph graph n)
-  (let ((d (- n (size graph))))
-    (unless (negative? d)
-      (struct-copy directed-graph graph
-		   [data (append (data graph)
-				 (make-list d '(() ())))]
-		   [size n]))))
+  (let ((edges (cdr (hash-ref (data graph) from (cons '() (hash))))))
+    (for ([(to data) edges])
+	 (func data to))))
