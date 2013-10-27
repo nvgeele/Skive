a020a8d1033977d610b05aa8f38de4695376a598
Nils Van Geele <nils.vgeele@gmail.com>
2013-10-27 02:06:10 +0200

    Created compound-node ADT
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

---
+++ src/compound-node.rkt
@@ -0,0 +1,38 @@
+#!racket
+
+(require "graph-boundary.rkt")
+
+(provide compound-node?
+	 make-compound-node
+	 add-graph
+	 subgraph-count)
+
+(struct compound-node (operation
+		       subgraphs order)
+	#:transparent)
+
+(define operation compound-node-operation)
+(define subgraphs compound-node-subgraphs)
+(define order compound-node-order)
+
+(define (make-compound-node)
+  (compound-node '() 0 '()))
+
+(define (add-graph cn graph [ord -1])
+  (cond ((not (graph-boundary? graph))
+	 (error "Not a graph boundary!"))
+	((or (= -1 ord)
+	     (<= 0 ord (subgraph-count cn)))
+	 (struct-copy compound-node cn
+		   [subgraphs (cons graph
+				  (subgraphs cn))]
+		   [order (if (= -1 ord)
+			    (append order `(,(subgraph-count cn)))
+			    (let-values (([a b] (split-at (order cn) ord)))
+			      (append a `(,(subgraph-count cn)) b)))]))
+	(else (error "Incorrect order"))))
+
+(define (subgraph-count cn)
+  (length (subgraphs cn)))
+
+;; TODO: set-order
