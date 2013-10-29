eb1a716534d088ec9269ac2c4bccb4d2044e6257
Nils Van Geele <nils.vgeele@gmail.com>
2013-10-29 20:45:45 +0100

    node: literal-node stuff added
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

--- src/node.rkt
+++ src/node.rkt
@@ -1,10 +1,22 @@
 #!racket
 
 (provide make-node node?
-	 opcode)
+	 opcode
+	 make-literal-node literal-node?
+	 value)
 
 (struct node (opcode)
 	#:transparent)
 
 (define make-node node)
 (define opcode node-opcode)
+
+(define (make-literal-node value)
+  (node `(literal ,value)))
+
+(define (literal-node? node)
+  (and (list? (opcode node))
+       (eq? 'literal (car (opcode node)))))
+
+(define (value node)
+  (cadr (opcode node)))
