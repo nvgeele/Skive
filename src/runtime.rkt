d0ee02c9c1878ac39275734da6892617681cde7e
Nils Van Geele <nils.vgeele@gmail.com>
2013-11-27 23:34:24 +0100

    New runtime functions
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

--- src/runtime.rkt
+++ src/runtime.rkt
@@ -5,228 +5,499 @@
 (provide generate-runtime-code)
 
 (define is_num
-  (~a
-"G	" type-check-fun-lbl "	\"is_num\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 1 0 0 0 0
-E	0 1	1 1	" typedval-lbl "
-{ Compound   2   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 2 2 6 0 0 1 0 0 0
-E	0 1	2 1	" typedval-lbl "
-N 3	141
-E	1 1	3 1	" bool-lbl "
-E	2 1	3 2	" bool-lbl "
-E	3 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_num\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 0 1 0 0 0 0	
+E	2 1	3 1	10	 
+{ Compound   4   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 4 2 7 0 1 0 0 0 0 0	
+E	2 1	4 1	10	 
+N 5	141	
+E	4 1	5 1	1	
+E	3 1	5 2	1	
+N 6	143
+E	5 1	6 5	1	
+E	6 1	0 1	10\n")
 
 (define is_int
-  (~a
-"G	" type-check-fun-lbl "	\"is_int\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 1 0 0 0 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_int\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 1 0 0 0 0 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_float
-  (~a
-"G	" type-check-fun-lbl "	\"is_float\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 0 1 0 0 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_float\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 0 1 0 0 0 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_null
-  (~a
-"G	" type-check-fun-lbl "	\"is_null\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 1 0 0 0 0 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_null\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 1 0 0 0 0 0 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_string
-  (~a
-"G	" type-check-fun-lbl "	\"is_string\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 0 0 1 0 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_string\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 0 0 1 0 0 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_bool
-  (~a
-"G	" type-check-fun-lbl "	\"is_bool\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 0 0 0 1 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_bool\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 0 0 0 1 0 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_cons
-  (~a
-"G	" type-check-fun-lbl "	\"is_cons\"
-{ Compound   1   2
-G	0
-L		0 1	" bool-lbl " \"false\"
-G	0
-L		0 1	" bool-lbl " \"true\"
-G	0
-L		0 1	" bool-lbl " \"error\"
-} 1 2 6 0 0 0 0 0 1
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_cons\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+L		0 1	1 \"false\"	
+G	0	
+L		0 1	1 \"true\"	
+G	0	
+L		0 1	1 \"error\"	
+} 3 2 7 0 0 0 0 0 1 0	
+E	2 1	3 1	10	 
+N 4	143
+E	3 1	4 5	1	
+E	4 1	0 1	10\n")
 
 (define is_false
-  (~a
-"G	" type-check-fun-lbl "	\"is_false\"
-{ Compound   1   2
-G	0
-N 1	139
-E	0 1	1 1	" bool-lbl "
-E	1 1	0 1	" bool-lbl "
-G	0
-L		0 1	" bool-lbl " \"false\"
-} 1 2 6 1 1 1 1 0 1
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" bool-lbl "\n"))
+"G	35	\"is_false\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+N 1	139	
+E	0 1	1 1	1	 
+N 2	143
+E	1 1	2 5	1	
+E	2 1	0 1	10	
+G	0	
+N 1	143
+L		1 5	1 \"false\"	
+E	1 1	0 1	10	
+} 3 2 7 1 1 1 1 0 1 1	
+E	2 1	3 1	10	 
+E	3 1	0 1	10\n")
+
+(define rt-cons
+"G	35	\"cons\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+N 3	105	
+E	1 2	3 1	26	 
+L		3 2	4 \"0\"	
+N 4	143	
+E	3 1	4 1	10	 
+E	2 1	4 2	10	 
+N 5	143
+E	4 1	5 6	18	 
+E	5 1	0 1	10\n")
 
 (define get_car
-  (~a
-"G	" unary-typedval-fun-lbl "	\"get_car\"
-{ Compound   1   2
-G	0
-N 1	144
-E	0 1	1 1	" conscell-lbl "
-E	1 1	0 1	" typedval-lbl "
-G	0
-L		0 1	" typedval-lbl " \"error\"
-} 1 2 6 1 1 1 1 1 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" typedval-lbl "\n"))
+"G	35	\"get_car\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+N 1	144	
+E	0 1	1 1	18	 
+E	1 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 3 2 7 1 1 1 1 1 0 1	
+E	2 1	3 1	10	 
+E	3 1	0 1	10\n")
 
 (define get_cdr
-  (~a
-"G	" unary-typedval-fun-lbl "	\"get_cdr\"
-{ Compound   1   2
-G	0
-N 1	144
-E	0 1	1 1	" conscell-lbl "
-E	1 2	0 1	" typedval-lbl "
-G	0
-L		0 1	" typedval-lbl " \"error\"
-} 1 2 6 1 1 1 1 1 0
-E	0 1	1 1	" typedval-lbl "
-E	1 1	0 1	" typedval-lbl "\n"))
-
-(define cons
-  (~a
-"G	" binary-typedval-fun-lbl "	\"cons\"
-N 1	143
-E	0 1	1 1	" typedval-lbl "
-E	0 2	1 2	" typedval-lbl "
-N 2	143
-E	1 1	2 6	" conscell-lbl "
-E	2 1	0 1	" typedval-lbl "\n"))
+"G	35	\"get_cdr\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+{ Compound   3   2
+G	0	
+N 1	144	
+E	0 1	1 1	18	 
+E	1 2	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 3 2 7 1 1 1 1 1 0 1	
+E	2 1	3 1	10	 
+E	3 1	0 1	10\n")
 
 (define plus
-  (~a
-"G	" binary-typedval-fun-lbl "	\"plus\"
+"G	35	\"plus\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+N 3	105	
+E	1 2	3 1	26	 
+L		3 2	4 \"0\"	
+{ Compound   4   2
+G	0	
 { Compound   1   2
-G	0
-{ Compound   1   2
-G	0
-N 1	141
-E	0 2	1 1	" int-lbl "
-E	0 1	1 2	" int-lbl "
+G	0	
+N 1	141	
+E	0 2	1 1	4	 
+E	0 1	1 2	4	 
 N 2	143
-E	1 1	2 2	" int-lbl "
-E	2 1	0 1	" typedval-lbl "
-G	0
-N 1	123
-E	0 2	1 1	" int-lbl "
-N 2	141
-E	1 1	2 1	" float-lbl "
-E	0 1	2 2	" float-lbl "
+E	1 1	2 2	4	 
+E	2 1	0 1	10	
+G	0	
+N 1	123	
+E	0 2	1 1	4	 
+N 2	141	
+E	1 1	2 1	3	 
+E	0 1	2 2	3	 
 N 3	143
-E	2 1	3 3	" float-lbl "
-E	3 1	0 1	" typedval-lbl "
-G	0
-L		0 1	" typedval-lbl " \"error\"
-} 1 2 6 2 0 1 2 2 2
-E	0 2	1 1	" typedval-lbl "
-E	0 1	1 2	" int-lbl "
-E	1 1	0 1	" typedval-lbl "
-G	0
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	4	 
+E	1 1	0 1	10	
+G	0	
 { Compound   1   2
-G	0
-N 1	123
-E	0 1	1 1	" int-lbl "
-N 2	141
-E	0 2	2 1	" float-lbl "
-E	1 1	2 2	" float-lbl "
+G	0	
+N 1	123	
+E	0 1	1 1	4	 
+N 2	141	
+E	0 2	2 1	3	 
+E	1 1	2 2	3	 
 N 3	143
-E	2 1	3 3	" float-lbl "
-E	3 1	0 1	" typedval-lbl "
-G	0
-N 1	141
-E	0 2	1 1	" float-lbl "
-E	0 1	1 2	" float-lbl "
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+N 1	141	
+E	0 2	1 1	3	 
+E	0 1	1 2	3	 
 N 2	143
-E	1 1	2 3	" float-lbl "
-E	2 1	0 1	" typedval-lbl "
-G	0
-L		0 1	" typedval-lbl " \"error\"
-} 1 2 6 2 0 1 2 2 2
-E	0 2	1 1	" typedval-lbl "
-E	0 1	1 2	" float-lbl "
-E	1 1	0 1	" typedval-lbl "
-G	0
-L		0 1	" typedval-lbl " \"error\"
-} 1 2 6 2 0 1 2 2 2
-E	0 1	1 1	" typedval-lbl "
-E	0 2	1 2	" typedval-lbl "
-E	1 1	0 1	" typedval-lbl "\n"))
+E	1 1	2 3	3	 
+E	2 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	3	 
+E	1 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 4 2 7 2 0 1 2 2 2 2	
+E	3 1	4 1	10	 
+E	2 1	4 2	10	 
+E	4 1	0 1	10\n")
+
+(define minus
+"G	35	\"minus\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+N 3	105	
+E	1 2	3 1	26	 
+L		3 2	4 \"0\"	
+{ Compound   4   2
+G	0	
+{ Compound   1   2
+G	0	
+N 1	135	
+E	0 2	1 1	4	 
+E	0 1	1 2	4	 
+N 2	143
+E	1 1	2 2	4	 
+E	2 1	0 1	10	
+G	0	
+N 1	123	
+E	0 2	1 1	4	 
+N 2	135	
+E	1 1	2 1	3	 
+E	0 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	4	 
+E	1 1	0 1	10	
+G	0	
+{ Compound   1   2
+G	0	
+N 1	123	
+E	0 1	1 1	4	 
+N 2	135	
+E	0 2	2 1	3	 
+E	1 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+N 1	135	
+E	0 2	1 1	3	 
+E	0 1	1 2	3	 
+N 2	143
+E	1 1	2 3	3	 
+E	2 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	3	 
+E	1 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 4 2 7 2 0 1 2 2 2 2	
+E	3 1	4 1	10	 
+E	2 1	4 2	10	 
+E	4 1	0 1	10\n")
+
+(define multiply
+"G	35	\"multiply\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+N 3	105	
+E	1 2	3 1	26	 
+L		3 2	4 \"0\"	
+{ Compound   4   2
+G	0	
+{ Compound   1   2
+G	0	
+N 1	152	
+E	0 2	1 1	4	 
+E	0 1	1 2	4	 
+N 2	143
+E	1 1	2 2	4	 
+E	2 1	0 1	10	
+G	0	
+N 1	123	
+E	0 2	1 1	4	 
+N 2	152	
+E	1 1	2 1	3	 
+E	0 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	4	 
+E	1 1	0 1	10	
+G	0	
+{ Compound   1   2
+G	0	
+N 1	123	
+E	0 1	1 1	4	 
+N 2	152	
+E	0 2	2 1	3	 
+E	1 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+N 1	152	
+E	0 2	1 1	3	 
+E	0 1	1 2	3	 
+N 2	143
+E	1 1	2 3	3	 
+E	2 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	3	 
+E	1 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 4 2 7 2 0 1 2 2 2 2	
+E	3 1	4 1	10	 
+E	2 1	4 2	10	 
+E	4 1	0 1	10\n")
+
+(define divide
+"G	35	\"divide\"	
+N 1	144	
+E	0 1	1 1	27	 
+N 2	105	
+E	1 2	2 1	26	 
+L		2 2	4 \"1\"	
+N 3	105	
+E	1 2	3 1	26	 
+L		3 2	4 \"0\"	
+{ Compound   4   2
+G	0	
+{ Compound   1   2
+G	0	
+N 1	122	
+E	0 2	1 1	4	 
+E	0 1	1 2	4	 
+N 2	143
+E	1 1	2 2	4	 
+E	2 1	0 1	10	
+G	0	
+N 1	123	
+E	0 2	1 1	4	 
+N 2	122	
+E	1 1	2 1	3	 
+E	0 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	4	 
+E	1 1	0 1	10	
+G	0	
+{ Compound   1   2
+G	0	
+N 1	123	
+E	0 1	1 1	4	 
+N 2	122	
+E	0 2	2 1	3	 
+E	1 1	2 2	3	 
+N 3	143
+E	2 1	3 3	3	 
+E	3 1	0 1	10	
+G	0	
+N 1	122	
+E	0 2	1 1	3	 
+E	0 1	1 2	3	 
+N 2	143
+E	1 1	2 3	3	 
+E	2 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 1 2 7 2 0 1 2 2 2 2	
+E	0 2	1 1	10	 
+E	0 1	1 2	3	 
+E	1 1	0 1	10	
+G	0	
+L		0 1	10 \"error\"	
+} 4 2 7 2 0 1 2 2 2 2	
+E	3 1	4 1	10	 
+E	2 1	4 2	10	 
+E	4 1	0 1	10\n")
 
 (define (generate-runtime-code)
   (string-append is_num is_int is_float
 		 is_null is_string is_bool 
 		 is_cons is_false get_car
-		 get_cdr cons plus))
+		 get_cdr rt-cons plus
+		 minus multiply divide))
