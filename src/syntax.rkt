2a33e769c1ea1501b4613176ade7e623842ac29d
Nils Van Geele <nils.vgeele@gmail.com>
2013-11-30 18:28:39 +0100

    added if? etc to syntax, also more exports and so on
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

--- src/syntax.rkt
+++ src/syntax.rkt
@@ -3,12 +3,14 @@
 (provide self-evaluating?
 	 application? appl-op appl-args
 	 let? let-definitions let-body
+	 lambda? lambda-args lambda-body
+	 if? if-condition if-consequent if-alternative
 	 or? and?)
 
 (define (self-evaluating? exp)
   (or (integer? exp)
       (string? exp)
-      (symbol? exp)
+      ;(symbol? exp)
       (boolean? exp)
       (null? exp)))
 
@@ -45,6 +47,18 @@
   (cddr exp))
 
 ;;;; If expressions
+(define (if? exp)
+  (and (list? exp)
+       (eq? (car exp) 'if)))
+
+(define (if-condition exp)
+  (cadr exp))
+
+(define (if-consequent exp)
+  (caddr exp))
+
+(define (if-alternative exp)
+  (cadddr exp))
 
 ;;;; and/or
 (define (or? exp)
