447b8586265ffe09bc446eda17df9ba372bebd7c
Nils Van Geele <nils.vgeele@gmail.com>
2013-12-02 15:16:11 +0100

    program->graphviz function because debugging and stuff
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

--- src/program.rkt
+++ src/program.rkt
@@ -1,10 +1,14 @@
 #!racket
 
 (require "natives.rkt")
+(require "graph-boundary.rkt")
+(require "node.rkt")
+(require "edge.rkt")
 
 (provide make-program
 	 add-boundary
-	 next-label)
+	 next-label next-label-str
+	 program->dot-file)
 
 (struct program (count boundaries)
 	#:transparent)
@@ -21,3 +25,36 @@
 (define (add-boundary prog boundary)
   (program (+ (program-count prog) 1)
 	   (cons boundary (program-boundaries prog))))
+
+(define (program->dot-file program)
+  (let loop ((cur (car (program-boundaries program)))
+	     (rem (cdr (program-boundaries program)))
+	     (res "digraph G {\n")
+	     (offset 0))
+    (let ((res (string-append
+		 res
+		 (foldl-nodes cur (~a "subgraph \"" (graph-boundary-name cur) "\" {\n")
+			      (lambda (label node res)
+				(let ((node-label (~a "node" (+ label offset)))
+				      (node-text
+					(~a "node " label "\\n"
+					    (cond ((literal-node? node)
+						   (~a "literal: "
+						       (value node)))
+						  ((simple-node? node)
+						   (~a "simple: "
+						       (opcode node)))
+						  (else "compound node")))))
+				  (~a res
+				      (foldl-edges cur label
+						   (~a node-label " [label=\""
+						       node-text "\"];\n")
+						   (lambda (edge res)
+						     (~a res
+							 node-label
+							 " -> node" (+ (edge-out-node edge) offset)
+							 " [label=\"" (edge-out-port edge) "\"];\n")))))))
+		 "}\n")))
+      (if (null? rem)
+	(string-append res "}")
+	(loop (car rem) (cdr rem) res (+ offset (label-counter cur)))))))
