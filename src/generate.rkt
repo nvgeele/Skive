00eb64b02b0ad2f0175763136900d72d23fb593e
Nils Van Geele <nils.vgeele@gmail.com>
2013-12-04 21:59:27 +0100

    Code cleanup
    Signed-off-by: Nils Van Geele <nils.vgeele@gmail.com>

--- src/generate.rkt
+++ src/generate.rkt
@@ -68,48 +68,17 @@
       (let*-values ([(gb-fail) (make-graph-boundary "")]
 		    [(gb-fail lit-err) (add-node gb-fail (make-literal-node "error"))]
 		    [(gb-fail) (add-edge gb-fail lit-err 1 0 1 frame-lbl)]
-		    
 		    [(gb-succ) (~> (make-graph-boundary "")
 				   (add-edge 0 1 0 1 frame-lbl))]
-
 		    [(gb frm-elm) (add-node gb (make-simple-node 144))]
-		    [(gb tagcase) (add-node gb (make-tagcase ;typedval-type-count
-						 `(,gb-fail ,gb-succ ,gb-fail)
-						 #(1 0)))]) ;; Assuming back-frame-idx is 1
+		    ;; Assuming back-frame-idx is 1
+		    ;; TODO: generate vector automatically or with procedure
+		    [(gb tagcase) (add-node gb (make-tagcase `(,gb-fail ,gb-succ ,gb-fail) #(1 0)))])
 	(loop (- frame 1)
 	      (~> gb
 		  (add-edge res 1 frm-elm 1 frame-lbl)
 		  (add-edge frm-elm frame-prev-idx tagcase 1 back-lbl))
 	      tagcase)))))
-
-
-      
-(define (generate-lookup* graph-boundary frame offset)
-  (let loop ((frame frame)
-	     (gb graph-boundary)
-	     (res 0))
-    (if (= frame 0)
-      (let*-values ([(gb res1) (add-node gb (make-simple-node 144))]  ;; RElements
-		    [(gb res2) (add-node gb (make-simple-node 105))]  ;; AElement
-		    [(gb lit1) (add-node gb (make-literal-node offset))])
-	#|(values (add-edge (add-edge gb res 1 ;; Assuming frame-idx is always 1!
-				      res1 1 frame-lbl)
-			    res1 frame-bind-idx
-			    res2 1 typedval-array-lbl)
-		  res2))|#
-      (values (~> gb
-		  (add-edge res 1 res1 1 frame-lbl) ;; Assuming back-frame-idx is always 1!
-		  (add-edge res1 frame-bind-idx res2 1 typedval-array-lbl)
-		  (add-edge lit1 1 res2 2 int-lbl))
-	      res2))
-    (let*-values ([(gb res1) (add-node gb (make-simple-node 144))]  ;; RElements
-		  [(gb res2) (add-node gb (make-simple-node 144))]) ;; RElements
-      (loop (- frame 1)
-	    (add-edge (add-edge gb res 1 ;; Assuming back-frame-idx is always 1!
-				res1 1 frame-lbl)
-		      res1 frame-prev-idx
-		      res2 1 back-lbl)
-	    res2)))))
 
 (define (generate-self-evaluating graph-boundary exp)
   (let*-values ([(lit-node build-node) (values (make-literal-node exp)
@@ -144,47 +113,6 @@
 		(add-edge bld1 1 bld2 typedval-func-idx closure-lbl))
 	    bld2)))
 
-#|(define (generate-args arg-exps graph-boundary program)
-  (if (null? arg-exps)
-    (values program graph-boundary '())
-    (let loop ((cur (car arg-exps))
-	       (rem (cdr arg-exps))
-	       (lst '())
-	       (gb graph-boundary)
-	       (program program))
-      (let-values ([(program gb res) (generate* cur gb program)])
-	(if (null? rem)
-	  (values program gb (reverse (cons res lst)))
-	  (loop (car rem) (cdr rem) (cons res lst) gb program))))))
-
-(define (generate-application exp graph-boundary program)
-  (let*-values ([(program gb op-res)
-		 (generate* (appl-op exp) graph-boundary program)]
-		[(program gb args)
-		 (generate-args (appl-args exp) gb program)]
-		[(gb bnd-bld) (add-node gb (make-simple-node 103))] ;; ABuild
-		[(gb tvl-elm) (add-node gb (make-simple-node 144))] ;; RElem
-		[(gb cls-elm) (add-node gb (make-simple-node 144))] ;; RElem
-		[(gb bck-bld) (add-node gb (make-simple-node 143))] ;; RBuild	
-		[(gb frm-bld) (add-node gb (make-simple-node 143))] ;; RBuild       
-		[(gb lit1) (add-node gb (make-literal-node "call"))]
-		[(gb call) (add-node gb (make-simple-node 120))]    ;; call
-		[(gb) (car (foldl (lambda (node c)
-				    (cons (add-edge (car c) node 1 bnd-bld (cdr c) typedval-lbl)
-					  (+ 1 (cdr c))))
-				  (cons gb 2) args))]
-		[(gb) (~> gb
-			  (add-edge op-res 1 tvl-elm 1 typedval-lbl)
-			  (add-edge tvl-elm typedval-func-idx cls-elm 1 closure-lbl)
-			  (add-edge cls-elm closure-framesize-idx bnd-bld 1 int-lbl)
-			  (add-edge cls-elm closure-env-idx bck-bld back-frame-idx frame-lbl)
-			  (add-edge bck-bld 1 frm-bld frame-prev-idx back-lbl);;fix?
-			  (add-edge bnd-bld 1 frm-bld frame-bind-idx typedval-array-lbl)
-			  (add-edge cls-elm closure-func-idx call 2 int-lbl)
-			  (add-edge frm-bld 1 call 3 frame-lbl)
-			  (add-edge lit1 1 call 1 call-function-lbl))])
-    (values program gb call)))|#
-
 (define (generate-args arg-exps graph-boundary program)
   (if (null? arg-exps)
     (values program graph-boundary '())
@@ -202,13 +130,10 @@
   (let*-values ([(gb-call) (make-graph-boundary "")]
 		[(gb-fail) (make-graph-boundary "")]
 		[(num-args) (length (appl-args exp))]
-
 		[(program gb op-res)
 		 (generate* (appl-op exp) graph-boundary program)]
 		[(program gb args)
 		 (generate-args (appl-args exp) gb program)]
-
-		;[(gb-call tvl-elm) (add-node gb-call (make-simple-node 144))]
 		[(gb-call cls-elm) (add-node gb-call (make-simple-node 144))]
 		[(gb-call bnd-lit) (add-node gb-call (make-literal-node "0"))]
 		[(gb-call bnd-bld) (add-node gb-call (make-simple-node 103))]
@@ -216,7 +141,6 @@
 		[(gb-call frm-bld) (add-node gb-call (make-simple-node 143))]
 		[(gb-call cll-lit) (add-node gb-call (make-literal-node "call"))]
 		[(gb-call call)    (add-node gb-call (make-simple-node 120))]
-
 		[(gb-call) (~> gb-call
 			       (add-edge 0 1 cls-elm 1 closure-lbl)
 			       (add-edge bnd-lit 1 bnd-bld 1 int-lbl)
@@ -227,35 +151,19 @@
 			       (add-edge frm-bld 1 call 3 frame-lbl)
 			       (add-edge cll-lit 1 call 1 call-function-lbl)
 			       (add-edge call 1 0 1 typedval-lbl))]
-
 		[(gb-call) (foldl (lambda (p gb)
 				    (add-edge gb 0 p bnd-bld p typedval-lbl))
 				  gb-call
 				  (range 2 (+ 2 num-args)))]
-
 		[(gb-fail err-lit) (add-node gb-fail (make-literal-node "error"))]
 		[(gb-fail) (add-edge gb-fail err-lit 1 0 1 typedval-lbl)]
-
-		[(cn) (make-tagcase ;typedval-type-count
-				    `(,gb-fail ,gb-call ,gb-fail)
-				    #(0 0 0 0 0 0 1))]
-
+		;; TODO: make sure vector gets generated automatically!
+		[(cn) (make-tagcase `(,gb-fail ,gb-call ,gb-fail) #(0 0 0 0 0 0 1))]
 		[(gb cn) (add-node gb cn)]
-
 		[(gb) (car (foldl (lambda (node c)
 				    (cons (add-edge (car c) node 1 cn (cdr c) typedval-lbl)
 					  (+ 1 (cdr c))))
 				  (cons gb 2) args))]
 		[(gb) (~> gb
-			  (add-edge op-res 1 cn 1 typedval-lbl))])#|
-			  (add-edge op-res 1 tvl-elm 1 typedval-lbl)
-			  (add-edge tvl-elm typedval-func-idx cls-elm 1 closure-lbl)
-			  (add-edge bnd-lit 1 bnd-bld 1 int-lbl)
-			  ;(add-edge cls-elm closure-framesize-idx bnd-bld 1 int-lbl)
-			  (add-edge cls-elm closure-env-idx bck-bld back-frame-idx frame-lbl)
-			  (add-edge bnd-bld 1 frm-bld frame-bind-idx typedval-array-lbl)
-			  (add-edge bck-bld 1 frm-bld frame-prev-idx back-lbl)
-			  (add-edge cls-elm closure-func-idx call 2 int-lbl)
-			  (add-edge frm-bld 1 call 3 frame-lbl)
-			  (add-edge cll-lit 1 call 1 call-function-lbl))])|#
+			  (add-edge op-res 1 cn 1 typedval-lbl))])
     (values program gb cn)))
--- src/translate.rkt
+++ src/translate.rkt
@@ -32,7 +32,6 @@
 (define (translate-boundary boundary [function-lbl function-lbl] [name #t])
   (let ((sorted (filter (lambda (i) (not (= i 0)))
 			(topological-sort boundary))))
-			;(range 1 (label-counter boundary)))))
     (~a "G " function-lbl
 	(if name
 	  (~a "\t\"" (graph-boundary-name boundary) "\"\n")
@@ -58,26 +57,6 @@
 				     (~a "E\t" (edge-in-node edge) " " (edge-in-port edge)
 					 "\t" (edge-out-node edge) " " (edge-out-port edge)
 					 "\t" (edge-type-lbl edge) "\n")))))))))
-
-(define (translate-boundary* boundary)
-  (~a
-    (foldl-nodes boundary
-		 (~a "G " function-lbl " \"" (graph-boundary-name boundary) "\"\n")
-		 (lambda (label node res)
-		   (string-append
-		     res
-		     (cond ((literal-node? node)
-			    (translate-literal-node boundary node label))
-			   ((simple-node? node)
-			    (translate-simple-node boundary node label))
-			   (else
-			     (translate-compound-node boundary node label))))))
-    (foldl-edges boundary 0 ""
-		 (lambda (edge res)
-		   (format "E\t~s ~s\t~s ~s\t~s\n"
-			   (edge-in-node edge) (edge-in-port edge)
-			   (edge-out-node edge) (edge-out-port edge)
-			   (edge-type-lbl edge))))))
 
 (define (translate-literal-node boundary node label)
   (let ((value (value node)))
@@ -136,18 +115,6 @@
 				    "\t" (edge-out-node edge) " " (edge-out-port edge)
 				    "\t" (edge-type-lbl edge) "\n"))))))))
 
-
-(define (make-call-function** program)
-  (~a "G " call-function-lbl "\t\"call\"\n"
-      "N 1\t143\n"
-      "L\t\t1 2\t4 \"1\"\n"
-      "E\t1 1\t0 1\t" typedval-lbl "\n")
-  (~a "G " call-function-lbl "\t\"call\"\n"
-      "N 1\t120\n"
-      "E\t0 2\t1 2\t" frame-lbl "\n"
-      "L\t\t1 1\t" function-lbl " \"cons\"\n"
-      "E\t1 1\t0 1\t" typedval-lbl "\n"))
-
 (define (make-call-function program)
   (let ((function-labels
 	  (append (map (lambda (n) (native-name (cdr n))) natives-list)
@@ -202,161 +169,8 @@
 	"E\t0 2\t3 3\t" frame-lbl "\n"
 	"E\t3 1\t0 1\t" typedval-lbl "\n\n")))
 
-#|(define (make-call-function program)
-  (let ((function-labels
-	  (append (map (lambda (n) (native-name (cdr n))) natives-list)
-		  (build-list (- (program-count program) (length natives-list))
-			      (lambda (i) (~a "proc" (+ i (length natives-list) -1)))))))
-    ;; We assume that there are at least 2 native functions...
-    (~a "G " call-function-lbl "\t\"call\"\n"
-	"N 1\t124\n"
-	"E\t0 1\t1 1\t" int-lbl "\n"
-	"L\t\t1 2\t" int-lbl " \"1\"\n"
-	"N 2\t129\n"
-	"E\t1 1\t2 1\t" bool-lbl "\n"
-	"{\n"
-	"G 0\n"
-	"E\t0 1\t0 1\t" int-lbl "\n"
-	"G 0\n"
-	"N 1\t120\n"
-	"L\t\t1 1\t" function-lbl " \"" (car function-labels) "\"\n"
-	"E\t0 3\t1 2\t" frame-lbl "\n"
-	"E\t1 1\t0 1\t" typedval-lbl "\n"
-	"G 0\n"
-	(let loop ((cur (cadr function-labels))
-		   (rem (cddr function-labels))
-		   (i 2))
-	  (~a 
-	      "N 1\t124\n"
-	      "E\t0 2\t1 1\t" int-lbl "\n"
-	      "L\t\t1 2\t" int-lbl " \"" i "\"\n"
-	      "N 2\t129\n"
-	      "E\t1 1\t2 1\t" bool-lbl "\n"
-	      "{\n"
-	      "G 0\n"	      
-	      "E\t0 1\t0 1\t" int-lbl "\n"
-	      "G 0\n"
-	      "N 1\t120\n"
-	      "L\t\t1 1\t" function-lbl " \"" cur "\"\n"
-	      "E\t0 3\t1 2\t" frame-lbl "\n"
-	      "E\t1 1\t0 1\t" typedval-lbl "\n"
-	      "G 0\n"
-	      (if (null? rem)
-		(~a "L\t\t0 1\t" typedval-lbl " \"error\"\n")
-		(loop (car rem) (cdr rem) (+ i 1)))
-	      "} 3 1 3 0 2 1\n"
-	      "E\t2 1\t3 1\t" int-lbl "\n"
-	      "E\t0 2\t3 2\t" int-lbl "\n"
-	      "E\t0 3\t3 3\t" frame-lbl "\n"
-	      "E\t3 1\t0 1\t" typedval-lbl "\n"))
-	"} 3 1 3 0 2 1\n"
-	"E\t2 1\t3 1\t" int-lbl "\n"
-	"E\t0 1\t3 2\t" int-lbl "\n"
-	"E\t0 2\t3 3\t" frame-lbl "\n"
-	"E\t3 1\t0 1\t" typedval-lbl "\n\n")))|#
-
-#|(define (make-call-function program)
-    (let ((funcs (append (map (lambda (n)
-				(native-name (cdr n)))
-			      natives-list)
-			 (build-list (- (program-count program)
-					(length natives-list))
-				     (lambda (i)
-				       (~a "proc" (+ i (length natives-list) -1)))))))
-      (~a "G " call-function-lbl "\t\"call\"\n"
-	  (let loop ((cur (car funcs))
-		     (rem (cdr funcs)))
-	    (~a "{\n"
-		"G 0\n"
-		"E\t0 1\t0 1\t" int-lbl "\n"
-		"G 0\n"
-		"N 1\t120\n"
-		"L\t\t1 1\t" function-lbl " \"" cur "\"\n"
-		"E\t0 2\t1 2\t" frame-lbl "\n"
-		"E\t1 1\t0 1\t" typedval-lbl "\n"
-		"G 0\n"
-		(if (null? rem)
-		  (~a "L\t\t0 1\t" typedval-lbl " \"error\"\n")
-		  (~a "N 1\t135\n"
-		      "E\t0 1\t1 1\t" int-lbl "\n"
-		      "L\t\t1 2\t" int-lbl " \"1\"\n"
-		      (loop (car rem) (cdr rem))
-		      #|"E\t1 1\t2 1\t" int-lbl "\n"
-		      "E\t0 2\t2 2\t" frame-lbl "\n"
-		      "E\t2 1\t0 1\t" typedval-lbl "\n"|#
-		      ))
-		"} 2 1 3 0 1 2\n"
-		"E\t0 1\t2 1\t" int-lbl "\n"
-		"E\t0 2\t2 2\t" frame-lbl "\n"
-		"E\t2 1\t0 1\t" typedval-lbl "\n")))))|#
-
-(define (make-call-function* program)
-  (~a "G " call-function-lbl "\t\"call\"\n"
-      "{ Compound 1 1\n"
-      "G 0\n"
-      "E\t0 1\t0 1\t4\n"
-      (foldl (lambda (label str)
-	       (~a str
-		   "G 0\n"
-		   "N 1\t120\n"
-		   "L\t\t1 1\t" function-lbl " \"" label "\"\n"
-		   "E\t0 2\t1 2\t" frame-lbl "\n"
-		   "E\t1 1\t0 1\t" typedval-lbl "\n"))
-	     ""
-	     (append (map (lambda (n)
-			    (native-name (cdr n)))
-			  natives-list)
-		     (build-list (- (program-count program)
-				    (length natives-list))
-				 (lambda (i)
-				   (~a "proc" (+ i (length natives-list) -1))))))
-      "} 1 1 "
-      (program-count program)
-      (foldl (lambda (i str)
-	       (~a str " " i))
-	     ""
-	     (range 0 (+ (program-count program) 1)))
-      "\n"
-      "E\t0 1\t1 1\t" int-lbl "\n"
-      "E\t0 2\t1 2\t" frame-lbl "\n"
-      "E\t1 1\t0 1\t" typedval-lbl "\n\n"))
-
 ;; The main function build ups the initial environment
 ;; and calls the main function with it.
-(define (make-main-function* program)
-  (~a "X " main-function-lbl "\t\"main\"\n"
-      ;; First we build an empty frame
-      "N 1\t143\n"
-      "L\t\t1 " back-null-idx "\t" null-lbl " \"nil\"\n"
-      "N 2\t103\n"
-      "L\t\t2 1\t" int-lbl " \"0\"\n"
-      "N 3\t143\n"
-      "E\t1 1\t3 " frame-prev-idx "\t" back-lbl "\n"
-      "E\t2 1\t3 " frame-bind-idx "\t" typedval-array-lbl "\n"
-      ;; Empty frame is now in node 3
-      ;; cnt := next free id
-      ;; ids := list of all typedval ids
-      (let-values ([(str cnt ids) (make-env-nodes)])
-	(~a str
-	    "N " cnt "\t103\n"
-	    "L\t\t" cnt " 1\t" int-lbl " \"" (length natives-list) "\"\n"
-	    (car
-	      (foldl (lambda (id cell)
-		       (cons
-			 (~a 
-			   (car cell)
-			   "E\t" id " 1\t" cnt " " (cdr cell) "\t" typedval-lbl "\n")
-			 (+ 1 (cdr cell))))
-		     (cons "" 2)
-		     ids))
-	    "N " (+ cnt 1) "\t143\n"
-	    "E\t1 1\t" (+ cnt 1) " " frame-prev-idx "\t" back-lbl "\n"
-	    "E\t" cnt " 1\t" (+ cnt 1) " " frame-bind-idx "\t" frame-lbl "\n"
-	    "N" (+ cnt 2) "\t120\n"
-	    "L\t\t" (+ cnt 2) " 1\t" function-lbl " \"entry\"\n"
-	    "E\t" (+ cnt 1) " 1\t" (+ cnt 2) " 2\t" typedval-array-lbl "\n"
-	    "E\t" (+ cnt 2) " 1\t0 1\t" typedval-lbl "\n"))))
-
 (define (make-main-function program)
   (~a "X " main-function-lbl "\t\"main\"\n"
       ;; First, we build a single frame, with no backlink
@@ -400,12 +214,12 @@
 (define (make-env-nodes)
   (let loop ((cur (car natives-list))
 	     (rem (cdr natives-list))
-	     (str "")(cnt 5)(ids '()) ;; CHANGE BACK TO 4 pls thx bai
+	     (str "")(cnt 5)(ids '()) ;; cnt depends on node with empty env!
 	     (id 0))
     (let ((str
 	    (~a str
 		"N " cnt "\t143\n"
-		"E\t4 1\t" cnt " " closure-env-idx "\t" frame-lbl "\n" ;; CHANGE BACK TO 3 pls thx bai
+		"E\t4 1\t" cnt " " closure-env-idx "\t" frame-lbl "\n" ;; Depends on node with empty env!
 		"L\t\t" cnt " " closure-func-idx "\t" int-lbl " \"" id "\"\n"
 		"L\t\t" cnt " " closure-args-idx "\t" int-lbl " \"" (native-inputs (cdr cur)) "\"\n"
 		"L\t\t" cnt " " closure-framesize-idx "\t" int-lbl " \"" (native-inputs (cdr cur)) "\"\n"
