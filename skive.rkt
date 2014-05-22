c6fed042c3b25048a2950791657e183fbc5da15c
Nils Van Geele <nils.vgeele@gmail.com>
2014-05-22 21:30:54 +0200

    Skive CLI: now able to only produce IF1 code

--- skive.rkt
+++ skive.rkt
@@ -4,11 +4,14 @@
          "src/compile.rkt")
 
 (define output-file (make-parameter #f))
+(define if1-mode (make-parameter #f))
+
 (define input-file
   (command-line
    #:program "skive"
    #:once-each
-   [("-o" "--out") file "output file" (output-file file)]
+   [("--IF1") "Do not compile, only produce IF1 code" (if1-mode #t)]
+   [("-o" "--out") file "Output file" (output-file file)]
    #:args (filename)
    filename))
 
@@ -23,9 +26,13 @@
 (define (compile input output-file)
   (let* ((skive-code (read-all input))
          (if1-code (compile-sequence-to-if1 skive-code)))
-    (compile-if1-to-native if1-code
-                           #:path (if output-file output-file "s.out")
-                           #:type 'exe)
+    (if (if1-mode)
+        (call-with-output-file (if output-file output-file "if1.out")
+          (lambda (out)
+            (display if1-code out)))
+        (compile-if1-to-native if1-code
+                               #:path (if output-file output-file "s.out")
+                               #:type 'exe))
     (void)))
 
 (call-with-input-file
