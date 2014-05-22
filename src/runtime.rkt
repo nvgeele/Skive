32a9558f8fefdcb530da75116826b5c086cbb402
Nils Van Geele <nils.vgeele@gmail.com>
2014-05-22 21:20:33 +0200

    Changed runtime loading code

--- src/runtime.rkt
+++ src/runtime.rkt
@@ -2,42 +2,56 @@
 
 (require "typing.rkt")
 
+;; All files containing the runtime functions
+(require "runtime/cons.rkt"
+         "runtime/divide.rkt"
+         "runtime/equal.rkt"
+         "runtime/get_car.rkt"
+         "runtime/get_cdr.rkt"
+         "runtime/is_false_nat.rkt"
+         "runtime/is_list.rkt"
+         "runtime/is_list_intern.rkt"
+         "runtime/minus.rkt"
+         "runtime/multiply.rkt"
+         "runtime/plus.rkt"
+         "runtime/apply.rkt"
+         "runtime/map-vector.rkt"
+         "runtime/vector-ref.rkt"
+         "runtime/vector-length.rkt"
+         "runtime/make-vector.rkt"
+         "runtime/error.rkt")
+
 (provide get-runtime-boundaries)
 
 (define runtime-list
-  '(cons
-    divide
-    equal
-    get_car
-    get_cdr
-    ;;is_bool
-    ;;is_cons
-    ;;is_false
-    is_false_nat
-    ;;is_float
-    ;;is_int
-    is_list
-    is_list_intern
-    ;;is_null
-    ;;is_num
-    ;;is_string
-    ;;is_symbol
-    ;;is_vector
-    minus
-    multiply
-    plus
-    apply
-    map-vector
-    vector-ref
-    vector-length
-    make-vector
-    error
-    ))
+  (list cons
+        divide
+        equal
+        get_car
+        get_cdr
+        ;;is_bool
+        ;;is_cons
+        ;;is_false
+        is_false_nat
+        ;;is_float
+        ;;is_int
+        is_list
+        is_list_intern
+        ;;is_null
+        ;;is_num
+        ;;is_string
+        ;;is_symbol
+        ;;is_vector
+        minus
+        multiply
+        plus
+        apply
+        map-vector
+        vector-ref
+        vector-length
+        make-vector
+        error
+        ))
 
 (define (get-runtime-boundaries)
-  (let*-values
-      ([(path ignore ignore2) (split-path (current-contract-region))]
-       [(path) (string-append (path->string path) "runtime/")])
-    (parameterize ([current-load-relative-directory path])
-      (for/list ([func runtime-list])
-        (eval `(dynamic-require ,(~a func ".rkt") ',func))))))
+  runtime-list)
