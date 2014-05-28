accfd4bc825e9958e0af31f983d4b769c141e985
Nils Van Geele <nils.vgeele@gmail.com>
2014-05-29 00:44:48 +0200

    Fixed #11 and minor refactor

--- src/analyse.rkt
+++ src/analyse.rkt
@@ -12,7 +12,7 @@
     (cond ((symbol? exp)
            (if (eq? exp 'null)
                '()
-               (lookup-var exp env)))
+               (lexical-address env exp)))
           ((or (self-evaluating? exp)
                (quote? exp))
            exp)
@@ -26,10 +26,12 @@
              `(lambda ,(lambda-args exp)
                 ,@(analyse (lambda-body exp) env))))
           ((application? exp)
-           `(,(case (appl-op exp)
-                [(list) 'list]
-                [(vector) 'vector]
-                [else (analyse (appl-op exp) env)])
+           `(,(if (lookup-var env (appl-op exp))
+                  (lexical-address env (appl-op exp))
+                  (case (appl-op exp)
+                    [(list) 'list]
+                    [(vector) 'vector]
+                    [else (analyse (appl-op exp) env)]))
              ,@(map (lambda (arg)
                       (analyse arg env))
                     (appl-args exp))))
@@ -44,12 +46,17 @@
               ((null? (cdr list)) #f)
               (else (loop (cdr list) (+ idx 1)))))))
 
-(define (lookup-var exp env)
+(define (lookup-var env exp)
   (let loop ((env env)
              (frame 0))
     (let ((idx (list-index exp (cdr env))))
-      (cond (idx
-             `#(,frame ,idx))
-            ((null? (car env))
-             (error (~a "Symbol `" exp "' not found in env -- analyse")))
+      (cond (idx (cons frame idx))
+            ((null? (car env)) #f)
             (else (loop (car env) (+ frame 1)))))))
+
+(define (lexical-address env exp)
+  (let ((lookup (lookup-var env exp)))
+    (if lookup
+        `#(,(car lookup)
+           ,(cdr lookup))
+        (error (~a "Symbol `" exp "' not found in env -- analyse")))))
